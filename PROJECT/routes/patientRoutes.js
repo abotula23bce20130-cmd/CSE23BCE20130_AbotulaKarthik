0b6e99effc39f083ec663856d64e1b5cabc8f5bd
viratKohli18Us <karthikohli2119@gmail.com>
2026-06-18 20:43:33 +0530

    Final Project Zip File added

--- PROJECT/routes/patientRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { createPatient } from "../controllers/patientController.js";
-
-const patientRouter = express.Router();
-
-patientRouter.post("/", createPatient);
-
-export default patientRouter;
